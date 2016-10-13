fe7e2c172148a2f375f1db3d08d470f0a5768ef0
thebhaiwas <thebhaiwas@gmail.com>
2016-10-13 14:15:02 +0530

    AnnexA database complete

--- app/src/main/java/com/shubhamrana/recordingandreporting/Annex3A.java
+++ app/src/main/java/com/shubhamrana/recordingandreporting/Annex3A.java
@@ -1,12 +1,37 @@
 package com.shubhamrana.recordingandreporting;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class Annex3A extends AppCompatActivity {
+public class Annex3A extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etNameRHF;
+    private EditText etDate;
+    private EditText etNamePatient;
+    private EditText etAge;
+    private RadioButton rbM;
+    private RadioButton rbF;
+    private EditText etAddress;
+    private CheckBox cbPulmonary;
+    private CheckBox cbEPulmonary;
+    private EditText etSite;
+    private CheckBox cbDiagnosis;
+    private CheckBox cbRepeat;
+    private CheckBox cbFollowUp;
+    private EditText etPTN;
+    private EditText etNameOfficial;
+    private EditText etSIN;
+    private EditText etDateSputum;
+    private EditText etNameCollector;
+    private Button btnSubmit1;
+
+    private DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +39,84 @@ public class Annex3A extends AppCompatActivity {
         setContentView(R.layout.activity_annex3);
 
         initializeUI();
+
+        dbHandler = new DBHandler(this, null, null, 0);
     }
 
-    public void initializeUI() {
+    private void initializeUI() {
 
+        etNameRHF = (EditText) findViewById(R.id.etNameRHF);
+        etDate = (EditText) findViewById(R.id.etDate);
+        etNamePatient = (EditText) findViewById(R.id.etNamePatient);
+        etAge = (EditText) findViewById(R.id.etAge);
+        rbM = (RadioButton) findViewById(R.id.rbM);
+        rbF = (RadioButton) findViewById(R.id.rbF);
+        etAddress = (EditText) findViewById(R.id.etAddress);
+        cbPulmonary = (CheckBox) findViewById(R.id.cbPulmonary);
+        cbEPulmonary =(CheckBox) findViewById(R.id.cbEPulmonary);
+        etSite = (EditText) findViewById(R.id.etSite);
+        cbDiagnosis = (CheckBox) findViewById(R.id.cbDiagnosis);
+        cbRepeat = (CheckBox) findViewById(R.id.cbRepeat);
+        cbFollowUp = (CheckBox) findViewById(R.id.cbFollowUp);
+        etPTN = (EditText) findViewById(R.id.etPTN);
+        etNameOfficial = (EditText) findViewById(R.id.etNameOfficial);
+        etSIN = (EditText) findViewById(R.id.etSIN);
+        etDateSputum = (EditText) findViewById(R.id.etDateSputum);
+        etNameCollector = (EditText) findViewById(R.id.etNameCollector);
+        btnSubmit1 = (Button) findViewById(R.id.btnSubmit1);
+
+        btnSubmit1.setOnClickListener(this);
     }
 
-    public boolean isOnline() {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSubmit1:
+                submitData();
+                break;
+        }
+    }
+
+    private void submitData() {
+
+        ContentValues cv = new ContentValues();
+        cv.put(DBHandler.COLSA[0], 0);
+        cv.put(DBHandler.COLSA[1], etNameRHF.getText().toString());
+        cv.put(DBHandler.COLSA[2], etDate.getText().toString());
+        cv.put(DBHandler.COLSA[3], etNamePatient.getText().toString());
+        cv.put(DBHandler.COLSA[4], etAge.getText().toString());
+        String sex;
+        if(rbM.isSelected())
+            sex = "M";
+        else if(rbF.isSelected())
+            sex = "F";
+        else sex = "";
+        cv.put(DBHandler.COLSA[5], sex);
+        cv.put(DBHandler.COLSA[6], etAddress.getText().toString());
+        StringBuilder sb = new StringBuilder("");
+        if(cbPulmonary.isChecked())
+            sb.append(cbEPulmonary.getText());
+        sb.append(" ");
+        if(cbEPulmonary.isChecked())
+            sb.append(cbEPulmonary.getText());
+        cv.put(DBHandler.COLSA[7], sb.toString());
+        cv.put(DBHandler.COLSA[8], etSite.getText().toString());
+        StringBuilder sbb = new StringBuilder("");
+        if(cbDiagnosis.isChecked())
+            sbb.append(cbDiagnosis.getText());
+        sbb.append(" ");
+        if(cbRepeat.isChecked())
+            sbb.append(cbRepeat.getText());
+        sbb.append(" ");
+        if(cbFollowUp.isChecked())
+            sbb.append(cbFollowUp.getText());
+        cv.put(DBHandler.COLSA[9], sbb.toString());
+        cv.put(DBHandler.COLSA[10], etPTN.getText().toString());
+        cv.put(DBHandler.COLSA[11], etNameOfficial.getText().toString());
+        cv.put(DBHandler.COLSA[12], etSIN.getText().toString());
+        cv.put(DBHandler.COLSA[13], etDateSputum.getText().toString());
+        cv.put(DBHandler.COLSA[14], etNameCollector.getText().toString());
+
+        dbHandler.addRowA(cv);
     }
 }
--- app/src/main/java/com/shubhamrana/recordingandreporting/DBHandler.java
+++ app/src/main/java/com/shubhamrana/recordingandreporting/DBHandler.java
@@ -1,22 +1,40 @@
 package com.shubhamrana.recordingandreporting;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class DBHandler  extends SQLiteOpenHelper{
+
+    private Context context;
 
     private static final int VERSION = 1;
     private static final String DATABASE = "MyDB";
 
-    private static final String TABLEA = "Info";
+    private static final String TABLEA = "ANNEXA";
     private static final String COL0A = "Id";
-    private static final String COL1A = "Name";
-    private static final String COL2A = "Age";
-    private static final String COLSA[] = {COL0A, COL1A, COL2A};
+    private static final String COL1A = "NameRHF";
+    private static final String COL2A = "Date";
+    private static final String COL3A = "NamePatient";
+    private static final String COL4A = "Age";
+    private static final String COL5A = "Sex";
+    private static final String COL6A = "Address";
+    private static final String COL7A = "Disease";
+    private static final String COL8A = "Site";
+    private static final String COL9A = "Reason";
+    private static final String COL10A = "PTN";
+    private static final String COL11A = "NameOfficial";
+    private static final String COL12A = "SIN";
+    private static final String COL13A = "DateSputum";
+    private static final String COL14A = "NameCollector";
+    public static final String COLSA[] = {COL0A, COL1A, COL2A, COL3A, COL4A, COL5A, COL6A,
+        COL7A, COL8A, COL9A, COL10A, COL11A, COL12A, COL13A, COL14A};
 
     public DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE, null, VERSION);
+        this.context = context;
     }
 
     @Override
@@ -37,12 +55,12 @@ public class DBHandler  extends SQLiteOpenHelper{
         onCreate(db);
     }
 
-    public boolean addRowA() {
+    public boolean addRowA(ContentValues cv) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLEA, null, cv);
+        db.close();
 
-        return true;
-    }
-
-    public boolean delRowA() {
+        Toast.makeText(context, "Record submitted successfully", Toast.LENGTH_SHORT).show();
 
         return true;
     }
