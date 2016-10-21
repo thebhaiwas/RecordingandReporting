2b5079844c906a2606a1457c7b356cddcd5dc84e
thebhaiwas <thebhaiwas@gmail.com>
2016-10-21 08:19:24 +0530

    activity 3b

---
+++ app/src/main/java/com/shubhamrana/recordingandreporting/Annex3B.java
@@ -0,0 +1,13 @@
+package com.shubhamrana.recordingandreporting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Annex3B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_annex3_b);
+    }
+}
